8238c1659ef41309d0c75f1439776d3f43536d56
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-05-08 17:42:19 +0530

    Added Recent/Related Posts & Categories

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import useStyles from './styles'
 import { PostCard,Categories,PostWidget} from '../components'
+import {getPosts} from '../services'
+import { AsyncLocalStorage } from 'async_hooks'
 
-const posts = [
-  { title: 'React Testing', excerpt: ' Lorem Lipsum' },
-  { title: 'TailWind Testing', excerpt: ' Lorem Lipsum Donor' },
-]
 
-const Home: NextPage = () => {
+// const posts = [
+//   { title: 'React Testing', excerpt: ' Lorem Lipsum' },
+//   { title: 'TailWind Testing', excerpt: ' Lorem Lipsum Donor' },
+// ]
+
+
+export default function Home ({posts}){
   const classes = useStyles()
   return (
     <div className="container mx-auto mb-8 px-10 ">
@@ -20,7 +24,7 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
-          <PostCard post={post} key={post.title}/>
+          <PostCard post={post.node} key={post.title}/>
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
@@ -34,4 +38,11 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+
+export async function getStaticProps(){
+  const posts=(await getPosts())||[];
+  return {
+    props:{posts}
+  }
+}
+
