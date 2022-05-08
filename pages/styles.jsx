7440d4d6fe93867e7c221c5df3cb775e70df5e63
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-05-08 10:32:30 +0530

    next & tailwind init

---
+++ pages/styles.jsx
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core'
+export default makeStyles(()=>({
+    background:{
+        borderBottom:'10px solid rgba(0,255,0,0.5)',
+        
+    },
+    mainDiv:{
+        
+    }
+}))
