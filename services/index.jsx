7b00e884a40265ffd0de4da53e56a15b5a610c6e
Sourabh-Bharale <89797467+Sourabh-Bharale@users.noreply.github.com>
2022-05-09 17:33:36 +0530

    Added multimedia support

--- services/index.jsx
+++ services/index.jsx
@@ -37,8 +37,9 @@ export const getPosts = async () => {
   
     return result.postsConnection.edges;
   };
+  
 
-export const getRecentPosts = async () => {
+  export const getRecentPosts = async () => {
     const query = gql`
       query GetPostDetails() {
         posts(
@@ -59,7 +60,7 @@ export const getRecentPosts = async () => {
     return result.posts;
   };
 
-export const getSimilarPosts = async ({ slug, categories }) => {
+  export const getSimilarPosts = async (categories, slug) => {
     const query = gql`
       query GetPostDetails($slug: String!, $categories: [String!]) {
         posts(
@@ -81,18 +82,52 @@ export const getSimilarPosts = async ({ slug, categories }) => {
   };
   
 
-  export const getCategories= async ()=>{
-    const query=gql`
-        query GetCategories{
-            categories {
-                name
-                slug
-            }
-        }
-    `
-
+  export const getCategories = async () => {
+    const query = gql`
+      query GetGategories {
+          categories {
+            name
+            slug
+          }
+      }
+    `;
+  
     const result = await request(graphqlAPI, query);
   
     return result.categories;
+  };
 
-  }
+  export const getPostDetails = async (slug) => {
+    const query = gql`
+      query GetPostDetails($slug : String!) {
+        post(where: {slug: $slug}) {
+          title
+          excerpt
+          featuredImage {
+            url
+          }
+          author{
+            name
+            bio
+            photo {
+              url
+            }
+          }
+          createdAt
+          slug
+          content {
+            raw
+          }
+          categories {
+            name
+            slug
+          }
+          
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.post;
+  };
